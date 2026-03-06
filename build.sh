25f46f8d1b60409a92b61320454a57b9eed921ef
Eric Long <i@hack3r.moe>
2026-03-06 16:53:34 +0800

    add output to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 cuda_version=$1
 ubuntu_version=$2
@@ -6,7 +7,11 @@ ubuntu_version=$2
 [ -z "$cuda_version" ] || cuda_version=13.1.1
 [ -z "$ubuntu_version" ] || ubuntu_version=22.04
 
+tag=isolate-envs_cuda-"$cuda_version"-ubuntu-"$ubuntu_version"
+
 docker build ./image \
   --build-arg CUDA_VERSION="$cuda_version" \
   --build-arg UBUNTU_VERSION="$ubuntu_version" \
-  -t isolate-envs_cuda-"$cuda_version"-ubuntu-"$ubuntu_version"
+  -t "$tag"
+
+>&2 echo Successfully created Docker image "'$tag'"
